39a020f0572bc2a6833ab873904b4db4b2ce1944
Kevin Winkel <git@kwinkel.one>
2023-11-01 21:33:31 +0100

    feat: moar configuration

--- workshop.py
+++ workshop.py
@@ -3,11 +3,9 @@ import yaml
 from steamcmd import SteamCmd
 
 game_id = 107410
-steamcmd_path = os.path.join('/','home','kevin','steamcmd')
-gameserver_path = os.path.join('/','home','kevin','mygameserver')
 
 with open("config.yml", "r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
-steamcmd = SteamCmd(steamcmd_path)
-steamcmd.install_workshop_collection(game_id, config['collection']['id'], gameserver_path, user=config['steam']['auth']['username'], validate=config['steam']['validate'])
+steamcmd = SteamCmd(config['steam']['path'])
+steamcmd.install_workshop_collection(game_id, config['arma3']['mods']['collection']['id'], config['arma3']['server']['path'], user=config['steam']['auth']['username'], validate=config['steam']['validate'])
