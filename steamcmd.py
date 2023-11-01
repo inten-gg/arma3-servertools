1aa1798a7bd5529b754ecf44c754af76703d72cb
Kevin Winkel <git@kwinkel.one>
2023-11-01 22:02:33 +0100

    feat: create symlinks for downloaded workshop content

--- steamcmd.py
+++ steamcmd.py
@@ -7,13 +7,13 @@ class SteamCmdException(Exception):
 
 class SteamCmd(object):
 
-    def __init__(self, install_path):
+    def __init__(self, install_path, executable):
         self.install_path = install_path
         
         if not os.path.isdir(self.install_path):
             raise SteamCmdException('Install path is not a directory or does not exist: {}'.format(self.install_path))
 
-        self.steamcmd_exe = os.path.join(self.install_path, 'steamcmd.sh')
+        self.steamcmd_exe = os.path.join(self.install_path, executable)
 
     def install_workshop_collection(self, gameid, workshop_collection_id, game_install_dir, user='anonymous', validate=False):
         if validate:
@@ -29,8 +29,10 @@ class SteamCmd(object):
 
         collection_response = requests.post('https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/', data='collectioncount=1&publishedfileids%5B0%5D={}'.format(workshop_collection_id), headers={ 'content-type': 'application/x-www-form-urlencoded' }).json()
 
+        workshop_item_ids = []
         for item in collection_response['response']['collectiondetails'][0]['children']:
             params.append('+workshop_download_item {} {}'.format(gameid, item['publishedfileid']))
+            workshop_item_ids.append(item['publishedfileid'])
 
         if validate != None:
             params.append('{}'.format(validate))
@@ -38,6 +40,15 @@ class SteamCmd(object):
         params.append('+quit')
 
         try:
-            return subprocess.check_call(params)
+            subprocess.check_call(params)
         except subprocess.CalledProcessError:
             raise SteamCmdException('Steamcmd was unable to run')
+        
+        for id in workshop_item_ids:
+            real_path = os.path.join(game_install_dir, 'steamapps', 'workshop', 'content', str(gameid), str(id))
+            link_path = os.path.join(game_install_dir, '@{}'.format(id))
+
+            if not os.path.islink(link_path):
+                os.symlink(real_path, link_path, True)
+
+        return 0
--- workshop.py
+++ workshop.py
@@ -7,5 +7,5 @@ game_id = 107410
 with open("config.yml", "r") as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
-steamcmd = SteamCmd(config['steam']['path'])
+steamcmd = SteamCmd(config['steam']['path'], config['steam']['executable'])
 steamcmd.install_workshop_collection(game_id, config['arma3']['mods']['collection']['id'], config['arma3']['server']['path'], user=config['steam']['auth']['username'], validate=config['steam']['validate'])
